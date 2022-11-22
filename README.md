4d32433261f0aad418f28ff2a4050fa823e9a032
Wesley Tsai <wesley7502@gmail.com>
2022-11-21 21:05:09 -0800

    Adding movement tracking script to the project. Updated README correspondingly

--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 EECS 571 project
 
 This project is a real time TTS device.
+
+be sure to install opencv by using
+
+```pip install opencv-contrib-python```
+
+
+To run the object tracking software, use
+```python3 .\object_track.py --tracker csrt```
---
+++ object_track.py
@@ -0,0 +1,86 @@
+
+'''
+Used to track a specific item
+'''
+
+# import the necessary packages
+import argparse
+import imutils
+import time
+import cv2
+
+ap = argparse.ArgumentParser()
+ap.add_argument("-t", "--tracker", type=str, default="kcf",
+	help="OpenCV object tracker type")
+args = vars(ap.parse_args())
+
+# initialize a dictionary that maps strings to their corresponding
+# OpenCV object tracker implementations
+OPENCV_OBJECT_TRACKERS = {
+    "csrt": cv2.TrackerCSRT_create,
+    "kcf": cv2.TrackerKCF_create,
+    "mil": cv2.TrackerMIL_create,
+}
+# grab the appropriate object tracker using our dictionary of
+# OpenCV object tracker objects
+
+tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
+
+# initialize the bounding box coordinates of the object we are going
+# to track
+initBB = None
+
+vs = cv2.VideoCapture(0)
+
+while True:
+	# grab the current frame, then handle if we are using a
+	# VideoStream or VideoCapture object
+	frame = vs.read()
+	frame = frame[1]
+	# check to see if we have reached the end of the stream
+	if frame is None:
+		break
+	# resize the frame (so we can process it faster) and grab the
+	# frame dimensions
+	(H, W) = frame.shape[:2]
+
+    # check to see if we are currently tracking an object
+	if initBB is not None:
+		# grab the new bounding box coordinates of the object
+		(success, box) = tracker.update(frame)
+		# check to see if the tracking was a success
+		if success:
+			(x, y, w, h) = [int(v) for v in box]
+			cv2.rectangle(frame, (x, y), (x + w, y + h),
+				(0, 255, 0), 2)
+		# initialize the set of information we'll be displaying on
+		# the frame
+		info = [
+			("Tracker", args["tracker"]),
+			("Success", "Yes" if success else "No"),
+		]
+		# loop over the info tuples and draw them on our frame
+		for (i, (k, v)) in enumerate(info):
+			text = "{}: {}".format(k, v)
+			cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+				cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+    	# show the output frame
+
+	cv2.imshow("Frame", frame)
+	key = cv2.waitKey(1) & 0xFF
+	# if the 's' key is selected, we are going to "select" a bounding
+	# box to track
+	if key == ord("s"):
+		# select the bounding box of the object we want to track (make
+		# sure you press ENTER or SPACE after selecting the ROI)
+		initBB = cv2.selectROI("Frame", frame, fromCenter=False,
+			showCrosshair=True)
+		# start OpenCV object tracker using the supplied bounding box
+		# coordinates
+
+		tracker.init(frame, initBB)
+	elif key == ord('q'):
+		break
+
+vs.stop()
+cv2.destroyAllWindows()
