45670d68d69bc8e402b156b8669596dca2f6e88f
Michael Wang <michwa@umich.edu>
2022-12-05 22:20:33 -0500

    Temporary commit to resolve pull conflict

--- orchestrator.py
+++ orchestrator.py
@@ -2,13 +2,10 @@
 Python file to combine the TTS and the movement tracking
 '''
 import time
-from tracker import Tracker
+import object_track
 
 def main_loop():
-    global prev_time
-    track = Tracker()
-    
-    prev_pos = track.get_values
+    global prev_time    
     # give the user 7 seconds to setup tracking
     time.sleep(7)
     while(True):
@@ -17,11 +14,10 @@ def main_loop():
         # if the time hits 5 seconds, get the x/y values
         if current_time - prev_time >= 5:
             prev_time = current_time
-            print(track.get_values())
 
 
 
 if __name__ == '__main__':
     global prev_time
     prev_time = time.time()
-    main_loop()
+    object_track.track_object()
