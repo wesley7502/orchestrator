53847fc2d754c0fec4e9275783f971e80fdd98bd
Wesley Tsai <wesley7502@gmail.com>
2022-12-05 23:40:23 -0500

    making the branch

--- orchestrator.py
+++ orchestrator.py
@@ -1,26 +1,42 @@
 '''
 Python file to combine the TTS and the movement tracking
+https://www.olivieraubert.net/vlc/python-ctypes/doc/
 '''
 import time
+from os import add_dll_directory
+add_dll_directory('C:\Program Files\VideoLAN\VLC')
+import vlc
 from tracker import Tracker
+from tts import createMP3
 
 def main_loop():
     global prev_time
+    media_player = vlc.MediaPlayer()
+    media = vlc.Media("speech.mp3")
+    media_player.set_media(media)
     track = Tracker()
 
     # give the user 7 seconds to setup tracking
     time.sleep(7)
+    media_player.play()
     while(True):
         current_time = time.time()
 
         # if the time hits 5 seconds, get the x/y values
-        if current_time - prev_time >= 5:
+        if current_time - prev_time >= 2:
             prev_time = current_time
-            print(track.get_values())
 
+            values = track.get_values()
+            x = values[0]
 
+            speed = x/125
+
+            print(speed)
+            #pause(media_player)
+            media_player.set_rate(speed)
 
 if __name__ == '__main__':
     global prev_time
+    createMP3('speech.txt')
     prev_time = time.time()
     main_loop()
--- tts.py
+++ tts.py
@@ -6,9 +6,24 @@ def playText(text,speed):
     engine.say(text)
     engine.runAndWait()
 
-if __name__ == '__main__':
-    f = open('speech.txt')
-    speed = 100
+def createMP3(filename):
+    f = open(filename)
+    engine = pyttsx3.init()
+    string = ''
     for line in f.readlines():
-        playText(line,speed)
-        speed += 25
+        string += ' '
+        string += line
+    engine.setProperty('rate', 100) 
+    engine.save_to_file(string, 'speech.mp3')
+    engine.runAndWait()
+
+
+if __name__ == '__main__':
+    # f = open('speech.txt')
+    # speed = 100
+    # for line in f.readlines():
+    #     words = line.split(' ')
+    #     for word in words:
+    #         playText(word,speed)
+    #         speed += 25
+    createMP3('speech.txt')
